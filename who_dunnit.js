52a31f272188d1ec429260c6f2e49174df2bf890
Chae Nicholson <chae.nicholson@gmail.com>
2019-06-17 21:48:02 +0100

    Home W6D1 completed - some further review / explanation required

--- who_dunnit.js
+++ who_dunnit.js
@@ -15,7 +15,9 @@ const declareMurderer = function() {
 
 const verdict = declareMurderer();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome: The murderer is Miss Scarlet
+// Why: scenario object is at same level / in same block as the test but murderer is available to declareMurderer function (lexical scope)
+// -> Correct
 
 // Episode 2
 const murderer = 'Professor Plum';
@@ -31,7 +33,10 @@ const declareMurderer = function() {
 changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome:  The murderer is Professor Plum
+// Why: const value cannot be changed in this case. 
+// -> Correct
+
 
 // Episode 3
 let murderer = 'Professor Plum';
@@ -46,7 +51,13 @@ console.log('First Verdict: ', firstVerdict);
 
 const secondVerdict = `The murderer is ${murderer}.`;
 console.log('Second Verdict: ', secondVerdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome :
+// First verdict: The murderer is Mrs Peacock
+// Why: This value is actually returned from declareMurderer:
+// Second verdict: The murderer is Professor Plum
+// Why: The assignment of Mrs Peacock is hidden so the value used is that available at same level / in same block
+// -> Correct
+
 
 // Episode 4
 let suspectOne = 'Miss Scarlet';
@@ -61,7 +72,13 @@ const declareAllSuspects = function() {
 const suspects = declareAllSuspects();
 console.log(suspects);
 console.log(`Suspect three is ${suspectThree}.`);
-// ---------- Predicted Outcome :
+// Predicted Outcome :
+// Suspects are Miss Scarlet, Professor Plum, Colonel Mustard
+// Why: Although the reassignment of suspect 3 is hidden, the updated list of suspects is actually passed back from the function.
+// Suspect 3 is Mrs Peacock
+// Why: Suspect 3 remains as Mrs Peacock as the reassignment is restricted to the declareAllSuspects function
+// -> Correct
+
 
 // Episode 5
 const scenario = {
@@ -81,7 +98,10 @@ const declareWeapon = function() {
 changeWeapon('Revolver');
 const verdict = declareWeapon();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome: The weapon is the candlestick
+// Why: The new value of weapon (revolver) is only available in declareWeapon
+// -> Wrong. Assuming this is because scenario is at same level / same block and we go from object level - reviewing next few answers...
+
 
 // Episode 6
 let murderer = 'Colonel Mustard';
@@ -103,7 +123,10 @@ const declareMurderer = function () {
 changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome : The murderer is colonel Mustard
+// Why: The assignments of different murderers are visible only within their functions
+// -> Wrong - but believe I see why - the assignment of Mr. Green and Mrs. White are to global variables and as Mrs White was last - she is the current value. Reviewing next exercises with this in mind...
+
 
 // Episode 7
 let murderer = 'Professor Plum';
@@ -131,7 +154,12 @@ const declareMurderer = function() {
 changeMurderer();
 const verdict = declareMurderer();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Original Predicted Outcome: The murderer is Professor Plum
+// Why: The assignments of different murderers are visible only within their functions
+// -> New answer: The murderer is Miss Scarlet
+// -> Why: Miss Scarlet is assigned to a global variable as the last operation (unexpectedOutcome) and so is the current value.
+// -> Wrong - need this explained!
+
 
 // Episode 8
 const scenario = {
@@ -168,7 +196,11 @@ const declareWeapon = function() {
 changeScenario();
 const verdict = declareWeapon();
 console.log(verdict);
-// ---------- Predicted Outcome :
+// Predicted Outcome: The weapon is Lead pipe
+// Why: The assignments of different murderers are visible only within their functions
+// -> New answer: Candlestick - based on what I believe is happening in Episode 5 - object attributes are updated even though at apparently outer block so the logic follows and we get to candlestick
+// -> Correct
+
 
 // Episode 9
 let murderer = 'Professor Plum';
@@ -183,7 +215,6 @@ const declareMurderer = function() {
 
 const verdict = declareMurderer();
 console.log(verdict);
-// ---------- Predicted Outcome :
-
-// Extensions
-// Make up your own episode!
+// Predicted Outcome : The murder is Professor Plum
+// Why: The assignment of a different murderer is visible only within the if block
+// -> Correct
